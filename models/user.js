e82300870a2b4066f87b6a98022e48e648b73002
Khaled <elgendy1204@gmail.com>
2018-07-09 00:31:30 +0200

    Continue model handling

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track');
+
+const schema = new Schema({ username: { type: String, required: true, unique: true } });
+schema.set('toJSON', { getters: true, virtuals: false });
+const User = mongoose.model('User', schema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,48 +1,62 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const cors = require('cors')
+const User = require('./models/user');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+app.use(cors());
 
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.static('public'));
 
-
-app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/api/exercise/new-user', (req, res) => {
+    const { username } = req.body;
+    if (!username) {
+        res.end('Path `username` is required.');
+    }
+    const user = new User({ username });
+    user.save((err, product) => {
+        if (err) {
+            if (err.code === 11000) {
+                res.end('username already taken');
+            }
+        }
+        res.json(user);
+    });
+});
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+    return next({ status: 404, message: 'not found' });
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+    let errCode, errMessage;
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400; // bad request
+        const keys = Object.keys(err.errors);
+        // report the first validation error
+        errMessage = err.errors[keys[0]].message;
+    } else {
+        // generic or custom error
+        errCode = err.status || 500;
+        errMessage = err.message || 'Internal Server Error';
+    }
+    res.status(errCode)
+        .type('txt')
+        .send(errMessage);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log('Your app is listening on port ' + listener.address().port);
+});
